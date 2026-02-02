3c7b720bf29200d323649915677ad7d4331a2781
darkwaffle <darkewaffle@gmail.com>
2026-02-02 11:54:21 -0500

    Implemented background draw functions

--- libraries/chart.lua
+++ libraries/chart.lua
@@ -7,17 +7,20 @@ ChartWidth = playersettings.ChartWidth or 100
 ChartHeight = playersettings.ChartHeight or 30
 
 function CreateChart(Visible)
+	CreateBackground(Visible)
 	CreateBars(Visible)
 	CreateLabels(Visible)
 end
 
 function DisplayChart(Visible)
+	DisplayBackground(Visible)
 	DisplayBars(Visible)
 	DisplayLabels(Visible)
 end
 
 function UpdateChart(TargetID)
 	if AttackLog[TargetID] then
+		DisplayBackground(true)
 		UpdateBars(TargetID)
 		UpdateLabels(TargetID)
 	end
