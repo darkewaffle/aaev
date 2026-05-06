dc874a6ec4025e5ad8b3b37f8bf9474c0fd55e91
darkwaffle <darkewaffle@gmail.com>
2026-05-06 14:07:08 -0400

    BackGround -> Background naming consistency. Select variables made public for use in labels.lua.

--- libraries/background.lua
+++ libraries/background.lua
@@ -1,20 +1,21 @@
-local EnableBackground = playersettings.BGDisplay
+EnableBackground = playersettings.BGDisplay
 if EnableBackground == nil then
 	EnableBackground = true
 end
 
-local BackgroundColor = playersettings.BGColor or Black
-local BackgroundPaddingX = playersettings.BGPaddingX or 0
-local BackgroundPaddingY = playersettings.BGPaddingY or 0
+BackgroundColor = playersettings.BGColor or Black
+BackgroundAlpha = playersettings.BGAlpha or 255
+
+BackgroundPaddingX = playersettings.BGPaddingX or 0
+BackgroundPaddingY = playersettings.BGPaddingY or 0
 local BackgroundOffsetX = playersettings.BGOffsetX or 0
-local BackGroundOffsetY = playersettings.BGOffsetY or 0
-local BackgroundAlpha = playersettings.BGAlpha or 255
+local BackgroundOffsetY = playersettings.BGOffsetY or 0
 local BackgroundName = "AAEV_Background"
 
 function CreateBackground(Visible)
 	if EnableBackground then
 		windower.prim.create(BackgroundName)
-		windower.prim.set_position(BackgroundName, ChartStartX - BackgroundPaddingX + BackgroundOffsetX, ChartStartY + BackgroundPaddingY - BackGroundOffsetY)
+		windower.prim.set_position(BackgroundName, ChartStartX - BackgroundPaddingX + BackgroundOffsetX, ChartStartY + BackgroundPaddingY - BackgroundOffsetY)
 		windower.prim.set_size(BackgroundName, ChartWidth + (2*BackgroundPaddingX) , -1 * (ChartHeight + (2*BackgroundPaddingY)))
 		windower.prim.set_color(BackgroundName, BackgroundAlpha, BackgroundColor[1], BackgroundColor[2], BackgroundColor[3])
 		windower.prim.set_visibility(BackgroundName, Visible)
